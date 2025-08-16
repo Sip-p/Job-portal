4e3bc6f1512b860aad3e4583d91c521a6fd04718
SipraaPanda <pandasipra300@gmail.com>
2025-08-16 06:59:39 +0530

    deplyment issues

--- server/controller/webhook.js
+++ server/controller/webhook.js
@@ -1,5 +1,5 @@
 import {Webhook} from 'svix'
-import User from '../models/User'
+import User from '../models/User.js'
 
 // /API CONTROLLER FUNCTION TO MANAGE CLERK USER WITH DATABASE
 export const clerkWebhooks=async(req,res)=>{
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
-import * as Sentry from '@sentry/node'
 import {clerkWebhooks} from './controller/webhook.js' 
 // Initialise Express
 const app=express();
@@ -22,7 +21,8 @@ app.get('/',(req,res)=>{
 app.post('/webhooks',clerkWebhooks)
 //port
 const PORT=process.env.PORT|| 5000
-Sentry.setupExpressErrorHandler(app);
-app.listen(PORT,()=>{
+ app.listen(PORT,()=>{
     console.log(`App is listening at port ${PORT}`)
 })
+
+// export default app;
